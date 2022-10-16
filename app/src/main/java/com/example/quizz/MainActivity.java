ab10c059f52f095d3cd12cf292ac810adbef6d50
Rafalrrr159 <rafal.rrr555@gmail.com>
2022-10-17 00:12:41 +0200

    Quiz app 2

--- app/src/main/java/com/example/quizz/MainActivity.java
+++ app/src/main/java/com/example/quizz/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.quizz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String QUIZ_TAG = "MainActivity";
+    public static final String activityLog = "Wywołana została metoda z cyklu życia: ";
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
@@ -30,12 +35,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(QUIZ_TAG, activityLog + "onCreate");
         setContentView(R.layout.activity_main);
 
+        if(savedInstanceState != null) {
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
+
+        questionTextView = findViewById(R.id.question_text_view);
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
-        questionTextView = findViewById(R.id.question_text_view);
+
+        setNextQuestion();
 
         trueButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -56,6 +68,43 @@ public class MainActivity extends AppCompatActivity {
                 setNextQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(QUIZ_TAG, activityLog + "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(QUIZ_TAG, activityLog + "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(QUIZ_TAG, activityLog + "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(QUIZ_TAG, activityLog + "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(QUIZ_TAG, activityLog + "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(QUIZ_TAG, activityLog + "onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
     }
 
     private void CheckAnswer(boolean userAnswer)
